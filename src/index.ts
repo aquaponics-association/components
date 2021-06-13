d1100b91abbd12bf34caf485396d4079009ff88c
Jonathan Reyes <me@jonathanreyes.com>
2021-06-13 06:37:46 +0300

    Added some basic fetch functionality and rendering

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,47 @@
+import { html, render } from 'lit-html';
+import * as qs from 'query-string';
+
 declare global {
   interface Window {
     AAOrganizationsList: typeof AAOrganizationsList;
   }
 }
 
-export class AAOrganizationsList {
+export interface AAImageFormat {
+  name: string;
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface AAImage {
+  id: number;
+  name: string;
+  caption?: string;
+  formats: {
+    thumbnail: AAImageFormat;
+    small: AAImageFormat;
+  };
+}
+
+export interface AATag {
+  id: number;
+  name: string;
+  description?: string;
+  uid: string;
+}
+
+export interface AAOrganization {
+  id: number;
+  name: string;
+  email?: string;
+  website?: string;
+  description?: string;
+  logo?: AAImage;
+  tags?: AATag[];
+}
+
+export default class AAOrganizationsList {
   constructor(selector: string) {
     const elements: NodeListOf<Element> = Array.prototype.slice.call(
       window.document.querySelectorAll(selector)
@@ -15,10 +52,15 @@ export class AAOrganizationsList {
     }
   }
 
-  async getData() {
-    // const tags = (el.getAttribute('data-tags') || '').split(',');
+  async getData(params: { tags?: string }) {
+    const options = {};
+
+    if (params.tags) options['tags.id'] = params.tags;
+
     const res = await fetch(
-      `https://api.aquaponicsassociation.org/organizations`
+      `https://api.aquaponicsassociation.org/organizations?${qs.stringify(
+        options
+      )}`
     );
 
     const json = await res.json();
@@ -26,8 +68,29 @@ export class AAOrganizationsList {
   }
 
   async process(el: Element) {
-    const data = await this.getData();
-    console.log(data);
+    const tags = el.getAttribute('data-tags');
+    const data: AAOrganization[] = await this.getData({ tags });
+
+    const template = data.map((org) => {
+      if (!org?.logo?.formats?.small?.url) return;
+
+      const logoUrl = `https://api.aquaponicsassociation.org${org?.logo?.formats?.small?.url}`;
+
+      return html`
+        <div class="aa-organization">
+          <div class="aa-logo">
+            <a href="${org.website || '#'}" rel="noopener" target="_blank">
+              <img src="${logoUrl}" alt="${org.name}" />
+            </a>
+          </div>
+          <a href="${org.website || '#'}" rel="noopener" target="_blank">
+            <h2 class="name">${org.name}</h2>
+          </a>
+        </div>
+      `;
+    });
+
+    render(template, el);
   }
 }
 
